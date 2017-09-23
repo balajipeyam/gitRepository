ffd7a849105a9218ee9428f8a9e8901688399211
Alagappan Muthaiah <amuthaia@visa.com>
2017-09-23 11:42:46 +0530

    basic template for thread by extending Thread class

---
+++ Practice/src/com/balaji/thread/ThreadByExtends.java
@@ -0,0 +1,17 @@
+package com.balaji.thread;
+
+public class ThreadByExtends extends Thread{
+	
+	@Override
+	public void run() {
+		
+	}
+
+	
+	
+	public static void main(String args[])
+	{
+		
+	}
+	
+}
